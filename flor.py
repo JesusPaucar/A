7286edf0cf25ceab9650b33301f6b7297f9a73f7
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-09-21 01:02:56 -0300

    Update flor.py

--- flor.py
+++ flor.py
@@ -1,18 +1,21 @@
-import cv2
+from PIL import Image, ImageDraw
 import numpy as np
 
 def draw_flower(petal_count):
     # Tamaño de la imagen y colores
     flower_size = 300
-    petal_colors = [(255, 215, 0), (255, 172, 51), (255, 215, 0)]  # Colores en formato BGR
+    petal_colors = [(255, 215, 0), (255, 172, 51), (255, 215, 0)]  # Colores en formato RGB
 
     # Crear una imagen en blanco
-    img = np.zeros((flower_size, flower_size, 3), dtype=np.uint8)
+    img = Image.new("RGB", (flower_size, flower_size), (255, 255, 255))
+
+    # Crear un objeto para dibujar en la imagen
+    draw = ImageDraw.Draw(img)
 
     # Dibujar el centro de la flor
     center_x = flower_size // 2
     center_y = flower_size // 2
-    cv2.circle(img, (center_x, center_y), 15, (0, 255, 255), -1)  # Color amarillo en formato BGR
+    draw.ellipse((center_x - 15, center_y - 15, center_x + 15, center_y + 15), fill=(255, 255, 0))  # Amarillo
 
     # Dibujar los pétalos
     petal_angles = np.linspace(0, 360, petal_count, endpoint=False)
@@ -20,7 +23,7 @@ def draw_flower(petal_count):
     for angle in petal_angles:
         x = center_x + int(0.2 * flower_size * np.sin(np.deg2rad(angle)))
         y = center_y + int(0.2 * flower_size * np.cos(np.deg2rad(angle)))
-        cv2.circle(img, (x, y), 15, petal_colors[np.random.randint(len(petal_colors))], -1)
+        draw.ellipse((x - 15, y - 15, x + 15, y + 15), fill=petal_colors[np.random.randint(len(petal_colors))])
 
     return img
 
@@ -28,9 +31,7 @@ def draw_flower(petal_count):
 flower_image = draw_flower(6)
 
 # Guardar la imagen como "flower.png"
-cv2.imwrite("flower.png", flower_image)
+flower_image.save("flower.png")
 
 # Mostrar la imagen
-cv2.imshow("Flor", flower_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+flower_image.show()
