fe225bc6292a3a6a92a9232ee00d4b64464ba693
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-09-21 00:37:46 -0300

    Update flor.py

--- flor.py
+++ flor.py
@@ -1,55 +1,35 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
-import numpy as np
 
-# Función para dibujar la flor
-def draw_flower(petal_count):
-    fig, ax = plt.subplots(figsize=(5, 5))
-    ax.set_aspect('equal')
-    ax.axis('off')
-
-    # Dibujar el centro de la flor
-    ax.scatter(0, 0, s=100, c='yellow', marker='o')
-
-    # Dibujar los pétalos
-    petal_colors = ['#FFD700', '#FFAC33', '#FFD700']
-    petal_angles = np.linspace(0, 360, petal_count, endpoint=False)
-
-    for angle in petal_angles:
-        x = 0.2 * np.sin(np.deg2rad(angle))
-        y = 0.2 * np.cos(np.deg2rad(angle))
-        petal = plt.Circle((x, y), 0.15, color=np.random.choice(petal_colors))
-        ax.add_patch(petal)
-
-    return fig
-
-# Función para enviar el mensaje de primavera
-def enviar_mensaje():
-    nombre = st.text_input("Nombre", "Andrea")
-    mensaje = f"¡Hola {nombre}!\n\n"
-    mensaje += "En esta hermosa temporada de primavera, quiero enviarte esta flor virtual\n como un símbolo de la belleza y la renovación que la primavera trae consigo.\n\n"
-    mensaje += "Espero que disfrutes de esta estación llena de colores y alegría. \nSonríe, al igual que la primavera lo hace con el mundo.\n\n"
-    mensaje += "Saludos,\n Jesus"
-
-    st.text("Mensaje:")
-    st.text(mensaje)
-
-# Crear la aplicación Streamlit
 def main():
-    st.title("Regalo Primaveral Interactivo")
+    st.title("Flor Interactiva")
 
-    # Solicitar al usuario el número de pétalos
-    petal_count = st.slider("Número de pétalos:", 7, 12, 8)
+    # Configuración de los colores
+    petal_color = st.color_picker("Color de los pétalos", "#FF69B4")
+    center_color = st.color_picker("Color del centro", "#FFD700")
 
-    # Generar la flor interactiva
-    fig = draw_flower(petal_count)
+    # Configurar el tamaño de la flor
+    flower_size = st.slider("Tamaño de la flor", 50, 300, 150)
 
-    # Mostrar la figura interactiva de Matplotlib
-    st.pyplot(fig)
+    # Crear el código SVG de la flor
+    svg_code = f"""
+    <svg height="{flower_size}" width="{flower_size}" xmlns="http://www.w3.org/2000/svg">
+        <!-- Centro de la flor -->
+        <circle cx="{flower_size // 2}" cy="{flower_size // 2}" r="{flower_size // 10}" fill="{center_color}" />
 
-    # Enviar el mensaje de primavera
-    enviar_mensaje()
+        <!-- Pétalos de la flor -->
+        <circle cx="{flower_size // 2}" cy="{flower_size // 4}" r="{flower_size // 4}" fill="{petal_color}" />
+        <circle cx="{flower_size // 4}" cy="{flower_size // 2}" r="{flower_size // 4}" fill="{petal_color}" />
+        <circle cx="{(flower_size // 4) * 3}" cy="{flower_size // 2}" r="{flower_size // 4}" fill="{petal_color}" />
+        <circle cx="{flower_size // 2}" cy="{(flower_size // 4) * 3}" r="{flower_size // 4}" fill="{petal_color}" />
+    </svg>
+    """
+
+    # Mostrar la flor SVG en Streamlit
+    st.write(svg_code, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
