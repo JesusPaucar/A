4e4bc9e29cb15da05061ea3546063d4c6aac6678
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-09-21 01:02:00 -0300

    Update flor.py

--- flor.py
+++ flor.py
@@ -1,34 +1,36 @@
-from PIL import Image, ImageDraw
+import cv2
 import numpy as np
 
 def draw_flower(petal_count):
     # Tamaño de la imagen y colores
     flower_size = 300
-    petal_colors = ['#FFD700', '#FFAC33', '#FFD700']
+    petal_colors = [(255, 215, 0), (255, 172, 51), (255, 215, 0)]  # Colores en formato BGR
 
     # Crear una imagen en blanco
-    img = Image.new("RGB", (flower_size, flower_size), "white")
-    draw = ImageDraw.Draw(img)
+    img = np.zeros((flower_size, flower_size, 3), dtype=np.uint8)
 
     # Dibujar el centro de la flor
     center_x = flower_size // 2
     center_y = flower_size // 2
-    draw.ellipse((center_x - 15, center_y - 15, center_x + 15, center_y + 15), fill='yellow')
+    cv2.circle(img, (center_x, center_y), 15, (0, 255, 255), -1)  # Color amarillo en formato BGR
 
     # Dibujar los pétalos
     petal_angles = np.linspace(0, 360, petal_count, endpoint=False)
 
     for angle in petal_angles:
-        x = center_x + 0.2 * flower_size * np.sin(np.deg2rad(angle))
-        y = center_y + 0.2 * flower_size * np.cos(np.deg2rad(angle))
-        petal = Image.new("RGB", (flower_size, flower_size), "white")
-        petal_draw = ImageDraw.Draw(petal)
-        petal_draw.ellipse((x - 15, y - 15, x + 15, y + 15), fill=np.random.choice(petal_colors))
-        img.paste(petal, (0, 0), petal)
+        x = center_x + int(0.2 * flower_size * np.sin(np.deg2rad(angle)))
+        y = center_y + int(0.2 * flower_size * np.cos(np.deg2rad(angle)))
+        cv2.circle(img, (x, y), 15, petal_colors[np.random.randint(len(petal_colors))], -1)
 
     return img
 
 # Generar una flor con 6 pétalos
 flower_image = draw_flower(6)
-flower_image.show()
-flower_image.save("flower.png")
+
+# Guardar la imagen como "flower.png"
+cv2.imwrite("flower.png", flower_image)
+
+# Mostrar la imagen
+cv2.imshow("Flor", flower_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
